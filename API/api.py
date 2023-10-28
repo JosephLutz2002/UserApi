ece38ed910c649135a03ad8ebc24215502e9ef70
Lutz, JKO, Mr [24696854@sun.ac.za] <24696854@sun.ac.za>
2023-10-28 14:52:43 +0200

    modifications

--- API/api.py
+++ API/api.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request,send_from_directory
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, create_refresh_token, get_jwt_identity
 from flask_jwt_extended.exceptions import JWTDecodeError
+from werkzeug.utils import secure_filename
 from datetime import timedelta
 import os
 import uuid
@@ -67,21 +68,23 @@ def protected():
     return {'message': f'Hello, {current_user}! This is a protected resource.'}, 200
 
 @app.route('/api/modules/addModule', methods=['POST'])
+@jwt_required()
 def addModule():
     module_name = request.json.get('module',None)
     module_year = request.json.get('year',None)
     module_code = request.json.get('code',None)
-    user_id = request.json.get('userid',None)
+    user_id = get_jwt_identity()
     module_id = str(uuid.uuid4())
     add_module(module_id,module_name,module_year,module_code,user_id)
     return {"id":module_id}
 
 @app.route('/api/modules/addAssignment',methods=['POST'])
+@jwt_required()
 def addAssignment():
     name = request.json.get('name',None)
     desc = request.json.get('desc',None)
     date = request.json.get('date',None)
-    userid = request.json.get('id',None)
+    userid = get_jwt_identity()
     module_id = request.json.get("module")
     weighting = request.json.get('weighting',None)
     Assignment_id = str(uuid.uuid4())
@@ -95,10 +98,11 @@ def addAssignment():
     return {"id":Assignment_id}
 
 @app.route('/api/modules/addTest',methods=['POST'])
+@jwt_required()
 def addTest():
     name = request.json.get('name',None)
     date = request.json.get('date',None)
-    user_id = request.json.get('userid',None)
+    user_id = get_jwt_identity()
     module_id = request.json.get('moduleid',None)
     weighting = request.json.get('weighting',None)
     test_id = str(uuid.uuid4())
@@ -106,22 +110,24 @@ def addTest():
     return{"id":test_id}
 
 @app.route('/api/modules/updateAssignment',methods=['POST'])
+@jwt_required()
 def updateAssignment():
     name = request.json.get('name',None)
     assign_id = request.json.get('assignid',None)
     module_id = request.json.get('moduleid',None)
-    user_id = request.json.get('userid',None)
+    user_id = get_jwt_identity()
     mark = request.json.get("mark",None)
     update_Assignment(assign_id,module_id,user_id,mark,name)
     mark = getMark(user_id,module_id)
     return {"mark":mark}
 
 @app.route('/api/modules/updateTest',methods=['POST'])
+@jwt_required()
 def updateTest():
     name = request.json.get('name',None)
     test_id = request.json.get('testid',None)
     module_id = request.json.get('module',None)
-    user_id = request.json.get('user',None)
+    user_id = get_jwt_identity()
     mark = request.json.get('mark',None)
     update_Test(mark,name,test_id,user_id,module_id)
     mark = getMark(user_id,module_id)
@@ -136,16 +142,17 @@ def refresh_token():
 
 
 @app.route('/api/modules/getModules',methods=['GET'])
+@jwt_required()
 def getNotes():
-    id = request.args.get('id')
-    print(id)
+    id = get_jwt_identity()
     modules = getAllModuleForUser(id)
     return {"Modules":modules}
 
 @app.route('/api/modules/deleteModule',methods=['POST'])
+@jwt_required()
 def deleteModule():
     moduleId = request.json.get('moduleid')
-    userid = request.json.get('userid')
+    userid = get_jwt_identity()
     print(moduleId)
     print(userid)
     deleteUserModule(moduleId,userid)
@@ -153,25 +160,28 @@ def deleteModule():
 
 
 @app.route('/api/modules/getAssignments',methods=['GET'])
+@jwt_required()
 def getAssignments():
     module_id = request.args.get('module_id')
-    user_id = request.args.get('user_id')
+    user_id = get_jwt_identity()
     assignments = getAllAssignments(module_id,user_id)
     print(assignments)
     return {'assignments':assignments}
 
 @app.route('/api/modules/getTests',methods=['GET'])
+@jwt_required()
 def getTests():
     module_id = request.args.get('module_id')
-    user_id = request.args.get('user_id')
+    user_id = get_jwt_identity()
     tests = getAllTests(module_id,user_id)
     print(tests)
     return {'tests':tests}    
 
 
 @app.route('/api/modules/average',methods=['GET'])
+@jwt_required()
 def getUserMark():
-    userid=request.args.get('user_id')
+    userid=get_jwt_identity()
     module_id = request.args.get('module_id')
     mark = getMark(userid,module_id)
     print(mark)
@@ -179,21 +189,61 @@ def getUserMark():
     
     
 @app.route('/api/modules/deleteAssignment',methods=['POST'])
+@jwt_required()
 def deleteAssignment():
     assign_id = request.json.get('assignid')
     module_id = request.json.get('moduleid')
-    user_id = request.json.get('userid')
+    user_id = get_jwt_identity()
     deleteAssig(user_id,assign_id,module_id)
     mark = getMark(user_id,module_id)
     return {"mark":mark}
 
 @app.route('/api/modules/deleteTest',methods=['POST'])
+@jwt_required()
 def deleteTest():
     test_id = request.json.get('testid')
     module_id = request.json.get('moduleid')
-    user_id = request.json.get('userid')
+    user_id = get_jwt_identity()
     deleteT(user_id,module_id,test_id)
     mark = getMark(user_id,module_id)
-    return {"mark":mark}    
+    return {"mark":mark}  
+
+UPLOAD_FOLDER = 'upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/upload', methods=['POST'])
+@jwt_required()
+def upload_file():
+    current_user = get_jwt_identity()
+    print(current_user)
+    if 'file' not in request.files:
+        return {'error': 'No file part'}
+    print(request.files)
+    file = request.files['file']
+    print('file is ', file)
+    if file.filename == '':
+        return {'error': 'No selected file'}
+
+    if file:
+        filename = secure_filename(current_user+'.png')
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return {'message': 'File uploaded successfully'}
+
+@app.route('/download')
+@jwt_required()
+def download_file():
+    current_user = get_jwt_identity()
+
+    if current_user:
+        file_path = os.path.join(UPLOAD_FOLDER, current_user+'.png')
+        if os.path.exists(file_path):
+            return send_from_directory(UPLOAD_FOLDER, current_user+'.png')        
+        else:
+            return "File not found", 404
+    else:
+        return "Missing 'filename' query parameter", 400
+  
 if __name__ == '__main__':
     app.run(debug=True)
